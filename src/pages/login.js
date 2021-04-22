3dfd597beb15534971e662f93aaaef526eadec7d
Junyu Zhu <alexzhuisme@gmail.com>
2021-04-21 22:24:22 -0700

    Login update

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import {Link, useHistory} from 'react-router-dom'
 import {useContext, useState, useEffect} from "react";
 import FirebaseContext from "../context/firebase";
+import * as ROUTES from '../constants/routes'
 
 export default function Login() {
   const history = useHistory();
@@ -12,7 +13,18 @@ export default function Login() {
   const [error, setError] = useState('')
   const isInvalid = password === '' || emailAddress === '';
   
-  const handleLogin = () => {}
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress,password)
+      history.push(ROUTES.DASHBOARD)
+    } catch (error){
+      setEmailAddress('')
+      setPassword('')
+      setError(error.message)
+    }
+  }
 
   useEffect(() => {
     document.title = 'Login - Instagram';
@@ -25,7 +37,7 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="iphone with Instagram app"/>
       </div>
       <div className="flex flex-col w-2/5">
-        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
           <h1 className="flex justify-center w-full">
             <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4"/>
           </h1>
@@ -50,20 +62,16 @@ export default function Login() {
                 setPassword(target.value)
               }}
             />
-            <button disabled={isInvalid} type="submit" className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}>Login</button>
+            <button disabled={isInvalid} type="submit" className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}>Login</button>
           </form>
         </div>
-        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
-          <p className="text-sm">Don't have an account? {``}</p>
-          <Link to="/signup" className="font-bold text-blue-medium">Sign up</Link>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border rounded border border-gray-primary">
+          <p className="text-sm">Don't have an account? {``}
+            <Link to="/signup" className="font-bold text-blue-medium">Sign up</Link>
+          </p>
         </div>
       </div>
     </div>
   )
 }
 
-//text-red-primary -> hex
-//text-gray-base
-//border-gray-primary
-//bg-blue-medium
-//text-blue-medium
