d6226e86ca9ae41489e1816042512d0a54e8683a
Junyu Zhu <alexzhuisme@gmail.com>
2021-04-21 21:58:14 -0700

    Login update

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,4 @@
-import { useHistory } from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import {useContext, useState, useEffect} from "react";
 import FirebaseContext from "../context/firebase";
 
@@ -25,32 +25,38 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="iphone with Instagram app"/>
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4"/>
-        </h1>
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4"/>
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({target}) => {
-              setEmailAddress(target.value)
-            }}
-          />
-          <input
-            aria-label="Enter your password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({target}) => {
-              setPassword(target.value)
-            }}
-          />
-          <button disabled={isInvalid} type="submit" className={'bg-blue-medium'}/>
-        </form>
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({target}) => {
+                setEmailAddress(target.value)
+              }}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({target}) => {
+                setPassword(target.value)
+              }}
+            />
+            <button disabled={isInvalid} type="submit" className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}>Login</button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+          <p className="text-sm">Don't have an account? {``}</p>
+          <Link to="/signup" className="font-bold text-blue-medium">Sign up</Link>
+        </div>
       </div>
     </div>
   )
@@ -60,3 +66,4 @@ export default function Login() {
 //text-gray-base
 //border-gray-primary
 //bg-blue-medium
+//text-blue-medium
