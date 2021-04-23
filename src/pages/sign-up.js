e7dc4815a3693f36bf4db88e7793785dabf2a4a6
Junyu Zhu <alexzhuisme@gmail.com>
2021-04-22 20:10:37 -0700

    signup page update

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -3,10 +3,12 @@ import {useContext, useState, useEffect} from "react";
 import FirebaseContext from "../context/firebase";
 import * as ROUTES from '../constants/routes'
 
-export default function Login() {
+export default function SignUp() {
   const history = useHistory();
   const { firebase } = useContext(FirebaseContext)
 
+  const [username, setUsername] = useState('')
+  const [fullName, setFullName] = useState('')
   const [emailAddress, setEmailAddress] = useState('')
   const [password, setPassword] = useState('');
 
@@ -42,6 +44,26 @@ export default function Login() {
 
           <form onSubmit={handleSignUp} method="POST">
             <input
+              aria-label="Enter your username"
+              type="text"
+              placeholder="Username"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({target}) => {
+                setUsername(target.value)
+              }}
+              value={username}
+            />
+            <input
+              aria-label="Enter your full name"
+              type="text"
+              placeholder="Full Name"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({target}) => {
+                setFullName(target.value)
+              }}
+              value={fullName}
+            />
+            <input
               aria-label="Enter your email address"
               type="text"
               placeholder="Email address"
@@ -49,6 +71,7 @@ export default function Login() {
               onChange={({target}) => {
                 setEmailAddress(target.value)
               }}
+              value={emailAddress}
             />
             <input
               aria-label="Enter your password"
@@ -58,13 +81,14 @@ export default function Login() {
               onChange={({target}) => {
                 setPassword(target.value)
               }}
+              value={password}
             />
-            <button disabled={isInvalid} type="submit" className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}>Login</button>
+            <button disabled={isInvalid} type="submit" className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}>Sign Up</button>
           </form>
         </div>
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 border rounded border border-gray-primary">
-          <p className="text-sm">Don't have an account? {``}
-            <Link to="/signup" className="font-bold text-blue-medium">Sign up</Link>
+          <p className="text-sm">Have an account? {``}
+            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">Login</Link>
           </p>
         </div>
       </div>
