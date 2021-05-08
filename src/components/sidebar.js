0ae358fc278e87f0307568c1d9753f4278facfd3
Junyu Zhu <alexzhuisme@gmail.com>
2021-05-07 17:58:25 -0700

    sidebar update

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,4 +1,12 @@
+import useUser from "../hooks/use-user";
+
 export default function Sidebar(){
+  // const {
+  //   user: { fullName, username, userId}
+  // } = useUser()
+  const x = useUser()
+  console.log(x)
+
   return (
     <p>I'm sidebar</p>
   )
