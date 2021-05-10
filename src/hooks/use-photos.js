71542faf1bdb9df3ea97964881581900c68eefa3
Junyu Zhu <alexzhuisme@gmail.com>
2021-05-10 15:04:28 -0700

    timeline update

---
+++ src/hooks/use-photos.js
@@ -0,0 +1,33 @@
+import {useContext, useEffect, useState} from "react";
+
+import UserContext from "../context/user";
+import {getPhotos, getUserByUserId} from "../services/firebase";
+
+export default function usePhotos(){
+  const [photos, setPhotos] = useState(null)
+  const {
+    user: {uid: userId = ''}
+  } = useContext(UserContext)
+
+  useEffect(() => {
+    return () => {
+      async function getTimeLinePhotos() {
+        // example: [2,1,5] <- 2 being Raphael
+        const following = await  getUserByUserId(userId)
+        let followedUserPhotos = [];
+
+        //does the user actually follow people
+        if (following.length > 0) {
+          followedUserPhotos = await getPhotos(userId, following)
+        }
+      }
+
+      console.log(userId)
+
+      // getTimeLinePhotos
+    };
+  }, []);
+
+
+  return {photos}
+}
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -73,4 +73,33 @@ export async function updatedFollowedUserFollowers(
     })
 }
 
+export async function getPhotos(userId, following){
+  // [5,4,2] => following
+  const result = await  firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', 'in', following)
+    .get();
+
+  const userFollowedPhotos = result.doc.map((photo) => ({
+    ...photo.data(),
+    docId: photo.id
+  }))
+
+  const photosWithUserDetails = await Promise.all(
+    userFollowedPhotos.map(async (photo)=>{
+      let userLikedPhoto = false;
+      if (photo.likes.includes(userId)) {
+        userLikedPhoto = true
+      }
+      // photo.userId = 2
+      const user = await  getUserByUserId(photo.userId)
+      //raphael
+      const {username} = user[0];
+      return {username, ...photo, userLikedPhoto}
+    })
+  )
+
+  return photosWithUserDetails
+}
 
