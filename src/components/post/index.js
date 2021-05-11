355f30dcfbcac787c87e7c26d85640599ab86f26
Junyu Zhu <alexzhuisme@gmail.com>
2021-05-11 16:59:10 -0700

    timeline header update

---
+++ src/components/post/index.js
@@ -0,0 +1,32 @@
+import {useRef} from "react";
+import PropTypes from "prop-types";
+import Header from "./header";
+
+
+export default function Post({content}) {
+  // components
+  // -> header, image, actions (like & comment icons), footer, comment
+  
+  return (
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+      <Header username={content.username}/>
+    </div>
+    )
+
+
+}
+
+
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comment: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired,
+  })
+}
